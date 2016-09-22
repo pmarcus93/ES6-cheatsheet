7c6bc5c988cd86d21a76e4df1c60a02e850f452d
Marcus Pereira <pereiramarcus93@gmail.com>
2016-09-22 11:48:13 -0300

    - Added text to clarify that most of the  examples was extracted from the book. - Spread / Rest operator.

--- es6-cheatsheet.js
+++ es6-cheatsheet.js
@@ -3,7 +3,8 @@ Based on You Don't Know Javascript - ES6 & Beyond
 by Kyle Simpson (@getify)
 https://github.com/getify/You-Dont-Know-JS
 
-This file contains short snippets and explanations of ES6 code. */
+This file contains short snippets and explanations of ES6 code,
+extracted from the book.*/
 
 
 /***** SCOPE *****/
@@ -43,4 +44,51 @@ you can still modify them. */
 {
   const name = 'Marcus', lastName = 'Pereira';
   name = 'João'; // TypeError
+}
+
+
+/***** OPERATORS *****/
+
+// Spread / Rest (Operator ...)
+
+// Spread
+/* The operator ... can be used to spread/expand values from iterables (such
+as an array). */
+
+{
+
+  let numbers = [12, 19, 14];
+
+  function foo(x, y, z) {
+    console.log(x, y, z);
+  }
+
+  foo(...numbers); // 12 19 14
+
+}
+
+/* It can be used to expand other values as well
+(such as inside another array declaration).*/
+
+{
+
+  let someCharacters = ['Lara Croft', 'Kirby', 'Alan Wake'];
+  let allCharacters = ['Sonic', ...someCharacters, 'Mario', 'Link'];
+  console.log(allCharacters); //Sonic,Lara Croft,Kirby,Alan Wake,Mario,Link
+
+}
+
+// Rest
+/* The operator ... can be used to gather non-named arguments ('the rest')in
+a function declaration. Useful in cases that you don't know the exact number
+of params that the function will gonna receive. Provides a solid alternative
+to using the deprecated arguments array. */
+
+{
+  function logMyArguments(...args) {
+    console.log(args);
+  }
+
+  logMyArguments('Milkshake', 'Burger', 'Fries', 'Coke');//Milkshake,Burger,Fries,Coke
+
 }
